5afda95b42affe7ac2b3cb9f4d3f3ed5ae328aaf
falmar <daviddlavier@gmail.com>
2016-10-27 17:11:34 -0400

    add utility funcs

---
+++ utility.go
@@ -0,0 +1,31 @@
+// Copyright 2016 David Lavieri.  All rights reserved.
+// Use of this source code is governed by a MIT License
+// License that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func jsonDecode(r io.Reader, into interface{}) error {
+	return json.NewDecoder(r).Decode(into)
+}
+
+func jsonEncode(w http.ResponseWriter, from interface{}) error {
+	return json.NewEncoder(w).Encode(from)
+}
+
+func jsonErrorEncode(w http.ResponseWriter, err error, code int) {
+	log.Println(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	jsonEncode(w, map[string]string{
+		"error": err.Error(),
+	})
+}
