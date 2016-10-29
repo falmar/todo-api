3a16c1e1b2ff33d1b5129a9d0db396880041ad0a
falmar <daviddlavier@gmail.com>
2016-10-28 21:21:28 -0400

    add basic TODO type

---
+++ type_todo.go
@@ -0,0 +1,12 @@
+// Copyright 2016 David Lavieri.  All rights reserved.
+// Use of this source code is governed by a MIT License
+// License that can be found in the LICENSE file.
+
+package main
+
+// Todo type that discribes it
+type Todo struct {
+	ID        int64
+	Title     string
+	Completed bool
+}
